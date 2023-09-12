ca55c0bed6852e34affc70032a792d97388abc91
devaninigam <nigamdevani2029@gmail.com>
2023-09-12 08:50:15 +0530

    completede

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const GetData = () => {
+    const getItem = localStorage.getItem("Project")
+    if (getItem) {
+      return JSON.parse(getItem)
+    } else {
+      return []
+    }
+  }
+  const [stor, setStor] = useState(GetData())
+  const [inputData, setInputData] = useState({
+    project: "",
+    detail: "",
+    color: false
+  })
+  const inputHendler = (e) => {
+    setInputData({
+      ...inputData,
+      [e.target.name]: e.target.value
+    })
+    console.log(inputData)
+  }
+  const send = () => {
+    if (inputData !== "") {
+      setStor([
+        ...stor,
+        {
+          id: stor.length + 1,
+          project: inputData.project,
+          detail: inputData.detail,
+          color: inputData.color
+        }
+      ])
+    }
+    localStorage.setItem("Project", JSON.stringify(stor))
+  }
+  const Update = (index) => {
+    const updatedStor = [...stor];
+    if (updatedStor[index].color === false) {
+      updatedStor[index].color = true;
+    } else if (updatedStor[index].color === true) {
+      updatedStor[index].color = false;
+    }
+    setStor(updatedStor);
+    localStorage.setItem("Project", JSON.stringify(updatedStor));
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='input'>
+        <input type="text" name='project' placeholder='Project Name' onChange={inputHendler} />
+        <input type="text" name='detail' placeholder='Project Detail' onChange={inputHendler} />
+        <button onClick={send}> Set Project  </button>
+      </div>
+      <div className='map'>
+        <div className='row'>
+          {
+            stor.map((item, index) =>
+              <div className='col-3'>
+                <div className="box">
+                  <div className={item.color ? "green" : "red"} key={index}>
+                    <h3>{item.project}</h3>
+                    <p>{item.detail}</p> 
+                      <button className='mapBtn' onClick={() => Update(index)}>Edit</button> 
+                  </div>
+                </div>
+              </div>
+            )
+          }
+        </div>
+      </div>
     </div>
   );
 }
